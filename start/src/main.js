a535798ff98b4023ac969b8dfa5e4237cb8f1ecf
sebtn <sdebedout@gmail.com>
2017-05-12 15:47:08 -0400

    Install react and react-dom

--- start/src/main.js
+++ start/src/main.js
@@ -1,2 +1,16 @@
-const $ = require('jquery')
-$('#target').html('hey ya!!!, can you see this with the w flag???/')
+let React = require('react')
+let ReactDOM = require('react-dom')
+
+class Message extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{this.props.title}</h1>
+				<h3>{this.props.message}</h3>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<Message title="Email Seb" message="can you email me pls?"/>, 
+	document.getElementById('react-container'))
